40b7538bce2fb44445b670528d8ca37d6c9651c1
inbaeyoonkr <iby2455@gmail.com>
2021-11-27 19:32:27 +0900

    Use QuackCounter decorator for counting duck's quack

--- DuckSimulator.ts
+++ DuckSimulator.ts
@@ -46,12 +46,32 @@ class GooseAdapater implements Quackable {
   }
 }
 
+// 데코레이터 객체도 어댑터 객체와 마찬가지로 타겟 인터페이스를 구현해야 한다.
+class QuackCounter implements Quackable {
+  private duck: Quackable; // 감싸고자 하는 객체의 레퍼런스를 저장하기 위한 인스턴스 변수
+  static numberOfQuacks: number = 0;
+
+  constructor(duck: Quackable) {
+    this.duck = duck;
+  }
+
+  quack() {
+    // quack() 메서드가 호출되면 그 호출을 이 데코레이터 안의 Quackable 객체에게 위임한다.
+    this.duck.quack();
+    QuackCounter.numberOfQuacks++; // 이 데코레이터를 통해 추가된 행위
+  }
+
+  getQuacks() {
+    return QuackCounter.numberOfQuacks;
+  }
+}
+
 class DuckSimulator {
   run() {
-    const mallardDuck = new MallardDuck() as Quackable;
-    const redheadDuck = new RedHeadDuck() as Quackable;
-    const duckCall = new DuckCall() as Quackable;
-    const rubberDuck = new RubberDuck() as Quackable;
+    const mallardDuck = new QuackCounter(new MallardDuck()) as Quackable;
+    const redheadDuck = new QuackCounter(new RedHeadDuck()) as Quackable;
+    const duckCall = new QuackCounter(new DuckCall()) as Quackable;
+    const rubberDuck = new QuackCounter(new RubberDuck()) as Quackable;
     const gooseDuck = new GooseAdapater(new Goose()) as Quackable;
 
     console.log("Duck Simulator");
@@ -61,6 +81,8 @@ class DuckSimulator {
     duckCall.quack();
     rubberDuck.quack();
     gooseDuck.quack();
+
+    console.log(`The ducks quacked ${QuackCounter.numberOfQuacks} times.`);
   }
 
   simulate(duck: Quackable) {
